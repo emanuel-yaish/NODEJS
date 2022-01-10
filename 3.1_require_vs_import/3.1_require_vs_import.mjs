291408d8b299846b8b087505bec985c8a0d4ed74
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-10 13:24:49 +0200

    submit 3.1

--- 3.1_require_vs_import/3.1_require_vs_import.mjs
+++ 3.1_require_vs_import/3.1_require_vs_import.mjs
@@ -1,10 +1,19 @@
 // What is the difference between import and require?
+// import is a jsx syntax so it use module and
 
 // How can you enable using the import syntax using node js
+// need to define file with mjs or type module in packagejson
 
 // Give 2 node.js environment variables that are not available
 // when using the import syntax.
+// 1. require
+// 2. module.exports
 
 // Create 3 functions using the export/import syntax.
 
 // Import the file system module using the import syntax.
+import { fn1, fn2, fn3 } from "./import.mjs";
+
+fn1();
+fn2();
+fn3();
---
+++ 3.1_require_vs_import/import.mjs
@@ -0,0 +1,9 @@
+export function fn1() {
+  console.log("func1");
+}
+export function fn2() {
+  console.log("func2");
+}
+export function fn3() {
+  console.log("func3");
+}
