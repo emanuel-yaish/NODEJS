7c7c7abee12427914c53c646afc726017eae9fb3
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-12 15:56:51 +0200

    submit 5.1 users

--- 5.1_users/app.js
+++ 5.1_users/app.js
@@ -5,8 +5,8 @@ const users = require("./users");
 yargs.command({
   command: "getUsers",
   describe: "get all users",
-  handler(argv) {
-    users.getUsers();
+  handler() {
+    console.log(users.getUsers());
   },
 });
 
@@ -27,6 +27,46 @@ yargs.command({
   },
   handler(argv) {
     users.addUser(argv.name, argv.email);
+  },
+});
+
+yargs.command({
+  command: "deleteUser",
+  describe: "Delete user",
+  builder: {
+    userID: {
+      describe: "User ID",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    users.deleteUser(argv.userID);
+  },
+});
+
+yargs.command({
+  command: "updateUser",
+  describe: "Delete user",
+  builder: {
+    userID: {
+      describe: "User ID",
+      demandOption: true,
+      type: "string",
+    },
+    name: {
+      describe: "User Name",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "User Email",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    users.updateUser(argv.userID, argv.name, argv.email);
   },
 });
 
--- 5.1_users/users.js
+++ 5.1_users/users.js
@@ -6,39 +6,61 @@ const loadUsers = () => {
     const dataBuffer = fs.readFileSync("users.json");
     const dataJson = dataBuffer.toString();
     return JSON.parse(dataJson);
+
+    // const dataBuffer = fs.readFileSync("users.json");
+    // const dataJson = dataBuffer.toString();
+    // const parsedDataJson = JSON.parse(dataJson);
+    // if (parsedDataJson.length >= 0) return parsedDataJson;
+    // else return [parsedDataJson];
   } catch (err) {
     return [];
   }
 };
 
 const saveUsers = (users) => {
+  console.log("saveUsers");
   const dataJson = JSON.stringify(users);
   fs.writeFileSync("users.json", dataJson);
 };
 
 const getUsers = () => {
-  console.log("get users");
+  return loadUsers();
 };
 
 const addUser = (name, email) => {
+  //   debugger;
   const users = loadUsers();
-
   users.push({ name, email, id: uniqid() });
-
   saveUsers(users);
 };
 
-const deletUser = (userID) => {
-  console.log("deletUser users");
+const deleteUser = (userID) => {
+  const users = loadUsers();
+  const newUsers = users.filter((user) => user.id !== userID);
+  if (users.length > newUsers.length) {
+    console.log("User deleted successfully");
+    saveUsers(newUsers);
+  } else {
+    console.log("User not found!");
+  }
 };
 
-const updateUser = (userID) => {
-  console.log("updateUser users");
+const updateUser = (userID, updatedName, updatedEmail) => {
+  const users = loadUsers();
+
+  const userToUpdate = users.find((user) => user.id === userID);
+
+  if (userToUpdate) {
+    if (updatedName) userToUpdate.name = updatedName;
+    if (updatedEmail) userToUpdate.email = updatedEmail;
+    console.log("User updated");
+    saveUsers(newUsers);
+  }
 };
 
 module.exports = {
   getUsers: getUsers,
   addUser: addUser,
-  deletUser: deletUser,
+  deleteUser: deleteUser,
   updateUser: updateUser,
 };
