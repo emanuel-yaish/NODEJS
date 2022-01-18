c2bdccb1913aba0082eb48224e158bb76efdbc21
Emanuel Yaish <emanuelyaish@gmail.com>
2022-01-18 23:20:45 +0200

    submit 15.2

--- 15.1_mongosh/index.js
+++ 15.1_mongosh/index.js
@@ -1,8 +1,8 @@
 // 1. Create a database called findMyRestaurant
-// use findMyRestaurant
+use findMyRestaurant
 
 // 2. Create a collection called restaurants
-// db.createCollection("restaurants");
+db.createCollection("restaurants");
 
 // 3. inside the collection create at least 5 restaurant
 // documents that include the following:
@@ -21,69 +21,75 @@
 // Only 3 different types of cuisine
 // Give different dates for your reviews.
 
-// db.restaurants.insertMany([
-//   {
-//     name: "resturantA",
-//     address: {
-//       city: "cityA",
-//       street: "streetA",
-//       coordinates: [-87, 344, 55.677],
-//     },
-//     cuisine: "cuisineA",
-//     kosher: true,
-//     reviews: [{ reviewDate: "12/4/2021", reviewScore: 5 }],
-//   },
-//   {
-//     name: "resturantB",
-//     address: {
-//       city: "cityB",
-//       street: "streetB",
-//       coordinates: [-37, 964, 50.677],
-//     },
-//     cuisine: "cuisineB",
-//     kosher: true,
-//     reviews: [{ reviewDate: "10/11/2021", reviewScore: 3 }],
-//   },
-//   {
-//     name: "resturantC",
-//     address: {
-//       city: "cityC",
-//       street: "streetC",
-//       coordinates: [-17, 164, 10.677],
-//     },
-//     cuisine: "cuisineC",
-//     kosher: true,
-//     reviews: [{ reviewDate: "21/5/2021", reviewScore: 4 }],
-//   },
-//   {
-//     name: "cuisineA",
-//     address: {
-//       city: "cityA",
-//       street: "streetD",
-//       coordinates: [-27, 164, 20.677],
-//     },
-//     cuisine: "cuisineA",
-//     kosher: true,
-//     reviews: [{ reviewDate: "22/1/2021", reviewScore: 1 }],
-//   },
-//   {
-//     name: "resturantE",
-//     address: {
-//       city: "cityB",
-//       street: "streetE",
-//       coordinates: [-71, 263, 44.671],
-//     },
-//     cuisine: "cuisineB",
-//     kosher: true,
-//     reviews: [{ reviewDate: "22/2/2021", reviewScore: 2 }],
-//   },
-// ]);
+db.restaurants.insertMany([
+  {
+    name: "resturantA",
+    address: {
+      city: "cityA",
+      street: "streetA",
+      coordinates: [-87, 344, 55.677],
+    },
+    cuisine: "cuisineA",
+    kosher: true,
+    reviews: [{ reviewDate: "12/4/2021", reviewScore: 5 }],
+  },
+  {
+    name: "resturantB",
+    address: {
+      city: "cityB",
+      street: "streetB",
+      coordinates: [-37, 964, 50.677],
+    },
+    cuisine: "cuisineB",
+    kosher: true,
+    reviews: [{ reviewDate: "10/11/2021", reviewScore: 3 }],
+  },
+  {
+    name: "resturantC",
+    address: {
+      city: "cityC",
+      street: "streetC",
+      coordinates: [-17, 164, 10.677],
+    },
+    cuisine: "cuisineC",
+    kosher: true,
+    reviews: [
+      { reviewDate: "21/5/2021", reviewScore: 4 },
+      { reviewDate: "12/5/2021", reviewScore: 4 },
+    ],
+  },
+  {
+    name: "cuisineA",
+    address: {
+      city: "cityA",
+      street: "streetD",
+      coordinates: [-27, 164, 20.677],
+    },
+    cuisine: "cuisineA",
+    kosher: true,
+    reviews: [{ reviewDate: "22/1/2021", reviewScore: 1 }],
+  },
+  {
+    name: "resturantE",
+    address: {
+      city: "cityB",
+      street: "streetE",
+      coordinates: [-71, 263, 44.671],
+    },
+    cuisine: "cuisineB",
+    kosher: true,
+    reviews: [
+      { reviewDate: "22/2/2021", reviewScore: 2 },
+      { reviewDate: "12/5/2021", reviewScore: 4 },
+    ],
+  },
+]);
 
 // show collection data
-// db.restaurants.find()
+db.restaurants.find()
 
 // delete collection
-// db.restaurants.drop();
+db.restaurants.drop();
 
 // show collections
-// show collections
+show collections
--- 15.2_restaurant_queries/index.js
+++ 15.2_restaurant_queries/index.js
@@ -98,18 +98,76 @@ db.restaurants.drop();
 // 2. forEach Queries
 // use the forEach cursor method to query the following:
 // 2.1 - Write a MongoDb query to print all restaurant names.
+
+db.restaurants.find().forEach(function (restaurants) {
+  print("restaurants name: " + restaurants.name);
+});
+
 // 2.2 - Write a MongoDb query to print all restaurant cities
+
+db.restaurants.find().forEach(function (restaurants) {
+  print("restaurants city: " + restaurants.address.city);
+});
+
 // 2.3 - Write a MongoDb query to print all restaurant coordinates
+
+db.restaurants.find().forEach(function (restaurants) {
+  print("restaurants coordinates: " + restaurants.address.coordinates);
+});
+
 // 3. Advanced Queries
 // 3.1 - Query for restaurant names that start with a specific
 // alphabet
+
+db.restaurants.find({ name: { $regex: /^r/, $options: "m" } });
+
 // 3.2 - Query how many documents you have from the restaurant
 // collection.
+
+db.restaurants.countDocuments({});
+
 // 3.3 - Write a MongoDb query to get restaurants that include
 // reviews from a specific date.
+
+db.restaurants.find({
+  reviews: { $in: [{ reviewDate: "22/1/2021", reviewScore: 1 }] },
+});
+
+db.restaurants.find({ "reviews.date": "3.3.16" });
+
+db.restaurants.find({ reviews: { $elemMatch: { reviewDate: "12/5/2021" } } });
+
 // 4. Aggregation operations
 // use the aggregation framework to query the following:
 // 4.1- Write a mongoDb query to display all restaurants average
 // score.
+
+db.restaurants.aggregate([
+  {
+    $project: { resturant: "$name", average: { $avg: "$reviews.reviewScore" } },
+  },
+]);
+
+db.restaurants.aggregate([
+  { $unwind: "$reviews" },
+  { $group: { _id: "$name", avgAge: { $avg: "$reviews.reviewScore" } } },
+]);
+
 // 4.2 - Write a mongoDb query to display a specific restaurant
 // average score
+
+db.restaurants.aggregate([
+  { $match: { name: "resturantE" } },
+  { $project: { average: { $avg: "$reviews.reviewScore" } } },
+]);
+
+db.restaurants.aggregate([
+  { $match: { name: "resturantE" } },
+  { $unwind: "$reviews" },
+  {
+    $group: {
+      _id: "$name",
+      average: { $avg: "$reviews.reviewScore" },
+    },
+  },
+]);
